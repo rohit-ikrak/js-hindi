7c35c3d7e8328cbe67db749d897bfa1997ad3789
ROHIT KARKI <86831121+rohit-ikrak@users.noreply.github.com>
2023-09-29 10:58:31 +0000

    a

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log("2" == 1);
+// console.log("2" >= 1);
+// console.log("2" <= 1);
+// console.log("02" >= 1);
+// console.log(null > 0); //F
+// console.log(null == 0); //F
+// console.log(null >= 0); //T
+
+// console.log(undefined == 0); //F for all >,<,>=,<=.
+
+// === ___ not only checks values but also checks datatypes
+
+console.log("2" == 2);  //t
+console.log("2" === 2); //f
+
+
+
+//avoid above comparisons... not recommended.
