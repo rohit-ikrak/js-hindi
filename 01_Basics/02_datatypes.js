07435f3a30242046f8bd3d6f73272572136672f6
ROHIT KARKI <86831121+rohit-ikrak@users.noreply.github.com>
2023-09-29 10:58:20 +0000

    reference datatypes update

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -1,9 +1,9 @@
-"use strict";   //treat all JS code as newer version
+// "use strict";   //treat all JS code as newer version
 
 // alert("3+3");  //we are using node.js, not browser
 
-console.log(2+3); 
-console.log("runnable");    // code readability should be high
+// console.log(2+3); 
+// console.log("runnable");    // code readability should be high
 
 // Number =>  2 to the power 53
 // BigInt => bigger numbers
@@ -13,4 +13,22 @@ console.log("runnable");    // code readability should be high
 // undefined =>
 // symbol=> unique (mostly used in react)
 
-console.log(typeof null);
+// console.log(typeof null);
+
+
+//reference type variables
+
+const heros = ["shaktiman", "krish", "karma"];
+
+let myObj = {
+    name: "rohit",
+    age: 25,
+}
+
+const myFunc = function(){
+    console.log("hello");
+}
+
+console.log(heros);
+console.log(heros[1]);
+myFunc();
