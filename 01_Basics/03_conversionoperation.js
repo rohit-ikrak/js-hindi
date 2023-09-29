0ea803a9e43492f88b7fdc738d8c711ad943fd92
ROHIT KARKI <86831121+rohit-ikrak@users.noreply.github.com>
2023-09-29 10:25:27 +0000

    datatype_conversion

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+// let score="33";
+// // console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let values = null
+// console.log(typeof(values));
+// let inNumber = Number(values);
+// console.log(values);
+// console.log(inNumber);
+
+// let val = undefined;
+// console.log(typeof(val));
+// let inNum = Number(val);
+// console.log(val);
+// console.log(inNum);
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// Boolean: 1=true ______ 0=false
+
+let number1 = 33
+let strnum1 = String(number1);
+console.log(strnum1);
+console.log(typeof(strnum1));
