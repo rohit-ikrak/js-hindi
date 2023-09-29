5037f04c0501ab0bf850bafeced319e8eb4f62ab
ROHIT KARKI <86831121+rohit-ikrak@users.noreply.github.com>
2023-09-29 10:37:09 +0000

    operations

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -19,14 +19,45 @@
 // console.log(inNum);
 
 
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// Boolean: 1=true ______ 0=false
+// // Boolean: 1=true ______ 0=false
 
-let number1 = 33
-let strnum1 = String(number1);
-console.log(strnum1);
-console.log(typeof(strnum1));
+// let number1 = 33
+// let strnum1 = String(number1);
+// console.log(strnum1);
+// console.log(typeof(strnum1));
+
+
+/*********************************Operation*************************/
+
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2*3);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = "rohit";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1+str3);
+// console.log(str3 + 1);
+console.log("a" + 1 + 1);
+console.log( 1 + 1 + "a" );
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter++); //101
+console.log(++gameCounter); //102
