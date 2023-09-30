ce7359b3bacfd1b156751471b527a3fff840f697
ROHIT KARKI <86831121+rohit-ikrak@users.noreply.github.com>
2023-09-30 07:48:33 +0000

    date and time

---
+++ 01_Basics/08_datentime.js
@@ -0,0 +1,39 @@
+// Dates
+
+// let today = new Date();
+// console.log(today);
+// console.log(today.toString());
+// console.log(today.toDateString());
+// console.log(today.toLocaleString());
+// console.log(typeof today);
+
+// let date = today.toString();
+// let sepdate = date.split(' ');
+// console.log(sepdate);
+
+// let timeline = {
+//     day: sepdate[0],
+//     mon: sepdate[1],
+//     date: sepdate[2],
+//     time: sepdate[3],
+//     timeline: sepdate[4],
+//     ran1: sepdate[5],
+//     ran2: sepdate[6],
+//     ran3: sepdate[7]
+// }
+
+// console.log(timeline.day);
+
+// let mydate = new Date(2023, 1, 1);
+// console.log(mydate);
+
+// let newdate = new Date("10-1-2023");
+// console.log(newdate);
+// console.log(newdate.getTime());
+// console.log(newdate.getMonth() + 1);
+// console.log(newdate.getDay());
+// let myTimeStamp = Date.now();
+
+//Timestamp
+// console.log(myTimeStamp);
+// console.log(Math.floor(myTimeStamp/(1000*60*60*24*365)));
